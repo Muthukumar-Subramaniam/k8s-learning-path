90b020f95196a1be6d4b839b17acfe46594f3dc7
Muthukumar-Subramaniam <msmkumar.eee@gmail.com>
2026-06-06 15:44:09 +0000

    update: k8s-workshop/simulate-yum-server-alma

--- k8s-workshop/simulate-yum-server-alma/apply.sh
+++ k8s-workshop/simulate-yum-server-alma/apply.sh
@@ -2,11 +2,8 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-if [ -z "${1:-}" ]; then
-    read -rp "Enter the domain name (e.g., user.internal): " domain
-else
-    domain="${1}"
-fi
+domain="${USER}.internal"
+
 nfs_server="tux2lab-engine.${domain}"
 
 for manifest in [0-9]-*.yaml; do
